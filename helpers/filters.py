1227da092dd9f2d01d16eb8f9cac9a3d89a4b184
x20xc <101093995+x20xc@users.noreply.github.com>
2022-05-12 12:48:36 +0300

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -22,7 +22,6 @@ from pyrogram import filters
 from config import COMMAND_PREFIXES
 
 
-other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 
 
 def command(commands: Union[str, List[str]]):
